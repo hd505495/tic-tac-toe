e8c785d9d8f7d1a879981fc128c599db6457c588
Hawk Dykes <hawkdykes@yahoo.com>
2020-07-29 12:23:51 -0500

    setup commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// player factory
+const player = (name, marker) => {
+
+  return {};
+};
+
+// game module
+const game = (() => {
+
+})();
+
+// display controller module
+const displayController = (() => {
+
+})();
